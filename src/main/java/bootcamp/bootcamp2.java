6491fab081d1d76f9b2dbbdd802f39d70d15ef64
Aishawii <aisha@sirka.io>
2022-09-04 22:25:59 +0700

    delete unused space and fix indentation

--- src/main/java/bootcamp/bootcamp2.java
+++ src/main/java/bootcamp/bootcamp2.java
@@ -3,20 +3,11 @@ package bootcamp;
 public class bootcamp2 {
     public static void main(String[] args) {
 
-
         int a =5;
         boolean hasil = a == 1 || a == 3 || a == 5 || a == 7 || a == 9;
         System.out.println("angka tersebut adalah = " + " " + a + " " + hasil);
-       a++;
+        a++;
         boolean hasil2 = a == 1 || a == 3 || a == 5 || a == 7 || a == 9;
         System.out.println("angka tersebut adalah = " + " " + a + " " + hasil2);
-
-
-
-
-
-
-
-
     }
 }
